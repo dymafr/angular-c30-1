1ada6e09ddd70637e241c370917ce2694fd1ff5d
dymafr <erwan1309@hotmail.com>
2021-10-08 11:09:36 +0200

    1 file modified

--- src/app/shared/store/auth.actions.spec.ts
+++ src/app/shared/store/auth.actions.spec.ts
@@ -2,28 +2,24 @@ import * as AuthActions from './auth.actions';
 
 describe('*** Auth actions ***', () => {
   describe('** TryFetchCurrentAction **', () => {
-    it('-> It should create a TryFetchCurrentAction action', () => {
+    it('-> should create a TryFetchCurrentAction action', () => {
       const action = AuthActions.tryFetchCurrentUserAction();
       expect(action).toEqual({
         type: AuthActions.tryFetchCurrentUserAction.type,
       });
     });
-
-    // it('-> It should create a SingupError action', () => {
-    //   const payload = {
-    //     error: 'error',
-    //   };
-    //   const action = new AuthActions.SignupError(payload);
-    //   expect({ ...action }).toEqual({
-    //     type: AuthActions.AuthActionTypes.SignupError,
-    //     payload,
-    //   });
-    // });
+  });
+  describe('** FetchCurrentUserSuccessAction **', () => {
+    it('-> should create a FetchCurrentUserSuccessAction action', () => {
+      const user = {
+        email: 'paul@dupond.fr',
+        name: 'Paul',
+      };
+      const action = AuthActions.fetchCurrentUserSuccessAction({user});
+      expect(action).toEqual({
+        type: AuthActions.fetchCurrentUserSuccessAction.type,
+        user
+      });
+    });
   });
 });
-
-// const payload = {
-//   email: 'paul@dupond.fr',
-//   name: 'Paul',
-//   password: '123456',
-// };
